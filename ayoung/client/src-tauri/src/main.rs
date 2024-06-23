d373c4038e7b113a39fb1a2725ca8f40f0efbaae
kwonah0 <42600987+kwonah0@users.noreply.github.com>
2024-06-24 00:38:08 +0900

    tauri - add menu

--- ayoung/client/src-tauri/src/main.rs
+++ ayoung/client/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +10,20 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // Create menu items
+    let menu = Menu::new()
+        .add_submenu(Submenu::new(
+            "App",
+            Menu::new().add_native_item(MenuItem::Quit)
+        ));
+
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
