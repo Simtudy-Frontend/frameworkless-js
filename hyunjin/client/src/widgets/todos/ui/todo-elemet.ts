67062c879c091f872957cd1c306d264f6d6bb58c
김현진 <guswls1846@naver.com>
2024-07-01 22:57:00 +0900

    modify: todo-item template 적용

--- hyunjin/client/src/widgets/todos/ui/todo-elemet.ts
+++ hyunjin/client/src/widgets/todos/ui/todo-elemet.ts
@@ -3,17 +3,19 @@ import { Todo } from "@shared/types";
 export const TodoElement = (todo: Todo) => {
   const { text, completed, id } = todo;
   return `
-  <li class="todo-item ${completed ? "completed" : ""}" data-id="${id}">
-        <div class="display-todo">
-          <label for="toggle-todo" class="toggle-todo-label visually-hidden">Toggle Todo</label>
-          <input id="toggle-todo" class="toggle-todo-input" type="checkbox" ${completed ? "checked" : ""} />
-          <span class="todo-item-text truncate-singleline" tabindex="0">${text}</span>
-          <button class="remove-todo-button" title="Remove Todo"></button>
-        </div>
-        <div class="edit-todo-container">
-          <label for="edit-todo" class="edit-todo-label visually-hidden">Edit todo</label>
-          <input id="edit-todo" class="edit-todo-input" />
-        </div>
-      </li>
+        <template id="todo-item">
+          <li class="todo-item ${completed ? "completed" : ""}" data-id="${id}">
+            <div class="display-todo">
+              <label for="toggle-todo" class="toggle-todo-label visually-hidden">Toggle Todo</label>
+              <input id="toggle-todo" class="toggle-todo-input" type="checkbox" ${completed ? "checked" : ""} />
+              <span class="todo-item-text truncate-singleline" tabindex="0">${text}</span>
+              <button class="remove-todo-button" title="Remove Todo"></button>
+            </div>
+            <div class="edit-todo-container">
+              <label for="edit-todo" class="edit-todo-label visually-hidden">Edit todo</label>
+              <input id="edit-todo" class="edit-todo-input" />
+            </div>
+          </li>
+        </template>
       `;
 };
