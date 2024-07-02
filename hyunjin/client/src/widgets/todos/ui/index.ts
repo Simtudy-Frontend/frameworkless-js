f488ce86dd35a16c67cccf43133146f0462d176f
김현진 <guswls1846@naver.com>
2024-07-02 22:17:10 +0900

    modify: 이벤트 위임 방식을 사용한 todo 삭제 기능 추가

--- hyunjin/client/src/widgets/todos/ui/index.ts
+++ hyunjin/client/src/widgets/todos/ui/index.ts
@@ -1,14 +1,21 @@
-import { State } from "@shared/types";
+import { Events, State } from "@shared/types";
 import { TodoElement } from "./todo-elemet";
 
-export const Todos = (targetElement: Element, { todos }: State) => {
-  const newTodoList = targetElement.cloneNode(true) as Element;
+export const Todos = (targetElement: Element, { todos }: State, { deleteItem }: Events) => {
+  const newTodoList = targetElement.cloneNode(true) as HTMLUListElement;
   newTodoList.innerHTML = "";
 
   todos.map(TodoElement)!.forEach((element) => {
     if (element) newTodoList.appendChild(element);
   });
 
+  newTodoList.addEventListener("click", (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+
+    if (target.matches("button.remove-todo-button")) {
+      deleteItem(Number(target.dataset.index));
+    }
+  });
   return newTodoList;
 };
 
