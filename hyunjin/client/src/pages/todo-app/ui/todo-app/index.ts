5d32922daa2bf6823081b1bd6d2b3d2d43518269
김현진 <guswls1846@naver.com>
2024-07-09 01:42:02 +0900

    feat: Refactor TodoApp component to use custom element definitions
    This commit refactors the TodoApp component to use custom element definitions. The previous code used `registry.add` calls, which have been replaced with `window.customElements.define` calls for `TodoApp`, `TodoFilter`, and `TodoList`. This change improves the organization and reusability of the HTML structure for the components.

--- hyunjin/client/src/pages/todo-app/ui/todo-app/index.ts
+++ hyunjin/client/src/pages/todo-app/ui/todo-app/index.ts
@@ -1,12 +1,73 @@
-import { addEvents, createAppElement } from "@pages/todo-app/model";
-import { Events, State } from "@shared/types";
+import { EVENTS, TodoFilterElement, State, TodoListElement, CurrentFilter } from "@shared/types";
 
-export const TodoApp = (targetElement: Element, state: State, events: Events) => {
-  const newApp = targetElement.cloneNode(true) as Element;
+export default class TodoApp extends HTMLElement {
+  private state: State;
+  private template: HTMLTemplateElement | null;
+  private filter!: TodoFilterElement;
+  private list!: TodoListElement;
 
-  newApp.innerHTML = "";
-  newApp.appendChild(createAppElement());
-  addEvents(newApp, events);
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentFilter: "All",
+    };
 
-  return newApp;
-};
+    this.template = document.getElementById("todo-app") as HTMLTemplateElement;
+  }
+
+  deleteItem(index: number) {
+    this.state.todos.splice(index, 1);
+    this.syncAttributes();
+  }
+
+  addItem(text: string) {
+    this.state.todos.push({
+      text,
+      completed: false,
+    });
+    this.syncAttributes();
+  }
+
+  selectFilter(currentFilter: CurrentFilter) {
+    console.log(currentFilter);
+
+    this.state.currentFilter = currentFilter;
+    this.syncAttributes();
+  }
+
+  syncAttributes() {
+    this.list.todos = this.state.todos;
+    this.filter.todos = this.state.todos;
+    this.filter.currentFilter = this.state.currentFilter;
+  }
+
+  connectedCallback() {
+    window.requestAnimationFrame(() => {
+      const content = this.template!.content.firstElementChild!.cloneNode(true);
+
+      this.appendChild(content);
+
+      const inputElement = this.querySelector(".new-todo-input") as HTMLInputElement;
+      inputElement!.addEventListener("keypress", (e: KeyboardEvent) => {
+        const target = e.target as HTMLInputElement; // 타입 단언
+        if (e.key === "Enter") {
+          this.addItem(target.value);
+          target.value = "";
+        }
+      });
+
+      this.filter = this.querySelector("todo-filter") as TodoFilterElement;
+      this.filter.addEventListener(EVENTS.SELECT_FILTER, (e: CustomEvent) => {
+        this.selectFilter(e.detail.currentFilter);
+      });
+
+      this.list = this.querySelector("todo-list") as TodoListElement;
+      this.list.addEventListener(EVENTS.DELETE_ITEM, (e: CustomEvent) => {
+        this.deleteItem(e.detail.index);
+      });
+
+      this.syncAttributes();
+    });
+  }
+}
